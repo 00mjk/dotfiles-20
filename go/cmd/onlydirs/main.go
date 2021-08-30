d94e5a3b4b0f8412776f36c541761b2415db52e1
BravoEcho <698828+bravoecho@users.noreply.github.com>
2021-08-30 16:46:07 +0100

    [cmd] Make a super-basic Go version of onlydirs script as bash too slow

---
+++ go/cmd/onlydirs/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+
+	var pwd string
+
+	args := os.Args[1:]
+	switch len(args) {
+	case 1:
+		pwd = args[0]
+	case 0:
+		var err error
+		pwd, err = os.Getwd()
+		if err != nil {
+			log.Fatal(err)
+		}
+	default:
+		log.Fatal("the only argument allowed is a base directory")
+	}
+
+	dirMap := make(map[string]struct{})
+
+	for sc.Scan() {
+		fpath := sc.Text()
+		dirpath := filepath.Dir(fpath)
+
+		if _, ok := dirMap[fpath]; ok {
+			continue
+		}
+		if _, ok := dirMap[dirpath]; ok {
+			continue
+		}
+
+		var fullfpath string
+		if filepath.IsAbs(fpath) {
+			fullfpath = fpath
+		} else {
+			fullfpath = filepath.Join(pwd, fpath)
+		}
+
+		finfo, err := os.Stat(fullfpath)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var dir string
+		if finfo.IsDir() {
+			dir = fpath
+		} else {
+			dir = dirpath
+		}
+
+		dirMap[dir] = struct{}{}
+
+		fmt.Println(dir)
+	}
+
+	if err := sc.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
