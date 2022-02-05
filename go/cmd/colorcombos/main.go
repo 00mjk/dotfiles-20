075bc8b0b8f9a0b516f4da3456494773550c80d5
BravoEcho <698828+bravoecho@users.noreply.github.com>
2022-02-05 00:32:05 +0000

    [utils] Update the colorcombos script to print ANSI palette codes

--- go/cmd/colorcombos/main.go
+++ go/cmd/colorcombos/main.go
@@ -48,19 +48,95 @@ func main() {
 		color.FgHiWhite,
 	}
 
-	printSimple("NORMAL / DEFAULT", normalForegrounds)
-	printSimple("BRIGHT / DEFAULT", brightForegrounds)
-	printSimple("DEFAULT / NORMAL", normalBackgrounds)
-	printSimple("DEFAULT / BRIGHT", brightBackgrounds)
-	printCombo("NORMAL / NORMAL", normalForegrounds, normalBackgrounds)
-	printCombo("NORMAL / BRIGHT", normalForegrounds, brightBackgrounds)
-	printCombo("BRIGHT / NORMAL", brightForegrounds, normalBackgrounds)
-	printCombo("BRIGHT / BRIGHT", brightForegrounds, brightBackgrounds)
+	fmt.Println()
+	fmt.Println("▶▶▶ NO COLOURS")
+	color.Unset()
+	fmt.Printf(" FG/BG █ ")
+	color.Unset()
+	color.Set(color.ReverseVideo)
+	fmt.Println(" BG/FG █ ")
+	color.Unset()
+	// fmt.Println()
+
+	printSimple("NORMAL FOREGROUND / DEFAULT BACKGROUND (AND REVERSE)", normalForegrounds)
+	printSimple("BRIGHT FOREGROUND / DEFAULT BACKGROUND (AND REVERSE)", brightForegrounds)
+	printSimple("DEFAULT FOREGROUND / NORMAL BACKGROUND (AND REVERSE)", normalBackgrounds)
+	printSimple("DEFAULT FOREGROUND / BRIGHT BACKGROUND (AND REVERSE)", brightBackgrounds)
+
+	printCombo("NORMAL FOREGROUNDS / NORMAL BACKGROUNDS", normalForegrounds, normalBackgrounds)
+	printCombo("NORMAL FOREGROUNDS / BRIGHT	BACKGROUNDS", normalForegrounds, brightBackgrounds)
+	printCombo("BRIGHT FOREGROUNDS / NORMAL	BACKGROUNDS", brightForegrounds, normalBackgrounds)
+	printCombo("BRIGHT FOREGROUNDS / BRIGHT	BACKGROUNDS", brightForegrounds, brightBackgrounds)
+}
+
+func toANSI(c color.Attribute) color.Attribute {
+	switch {
+	case c <= 37: // normal foreground
+		return c - 30
+	case c <= 47: // normal backgrund
+		return c - 40
+	case c <= 97: // bright foreground
+		return c - 82
+	case c <= 107: // bright backgrund
+		return c - 92
+	default:
+		panic("check the values")
+	}
 }
 
 func printTitle(text string) {
-	fmt.Println("————————————————————————————————————————————————————————————————————————")
-	fmt.Println(text)
+	// fmt.Println("————————————————————————————————————————————————————————————————————————")
+	// fmt.Println()
+	fmt.Println("▶▶▶", text)
+	// fmt.Println()
+}
+
+func printFGBG(c color.Attribute) {
+	color.Set(c)
+	defer color.Unset()
+
+	switch {
+	case c <= 37: // normal foreground
+		fmt.Printf(" %2v|BG █ ", toANSI(c))
+	case c <= 47: // normal backgrund
+		fmt.Printf(" FG|%-2v █ ", toANSI(c))
+	case c <= 97: // bright foreground
+		fmt.Printf(" %2v|BG █ ", toANSI(c))
+	case c <= 107: // bright backgrund
+		fmt.Printf(" FG|%-2v █ ", toANSI(c))
+	default:
+		panic("check the values")
+	}
+}
+
+func printFGBGReverse(c color.Attribute) {
+	color.Set(c, color.ReverseVideo)
+	defer color.Unset()
+
+	switch {
+	case c <= 37: // normal foreground
+		fmt.Printf(" BG|%-2v █ ", toANSI(c))
+	case c <= 47: // normal backgrund
+		fmt.Printf(" %2v|FG █ ", toANSI(c))
+	case c <= 97: // bright foreground
+		fmt.Printf(" BG|%-2v █ ", toANSI(c))
+	case c <= 107: // bright backgrund
+		fmt.Printf(" %2v|FG █ ", toANSI(c))
+	default:
+		panic("check the values")
+	}
+}
+
+func printSimple(title string, attributes []color.Attribute) {
+	printTitle(title)
+
+	for _, attr := range attributes {
+		printFGBG(attr)
+	}
+	fmt.Println()
+	for _, attr := range attributes {
+		printFGBGReverse(attr)
+	}
 	fmt.Println()
 }
 
@@ -70,33 +146,10 @@ func printCombo(title string, normalForegrounds, normalBackgrounds []color.Attri
 	for _, bg := range normalBackgrounds {
 		for _, fg := range normalForegrounds {
 			color.Set(fg, bg)
-			fmt.Printf(" %v/%v ", fg, bg)
+			fmt.Printf(" %2v/%-2v █ ", toANSI(fg), toANSI(bg))
 			color.Unset()
 		}
 		fmt.Println()
 	}
-}
-
-func printSimple(title string, attributes []color.Attribute) {
-	printTitle(title)
-
-	color.Unset()
-	fmt.Printf(" 00 ")
-	color.Unset()
-	for _, attr := range attributes {
-		color.Set(attr)
-		fmt.Printf(" %v ", attr)
-		color.Unset()
-	}
-	fmt.Println()
-
-	color.Set(color.ReverseVideo)
-	fmt.Printf(" 00 ")
-	color.Unset()
-	for _, attr := range attributes {
-		color.Set(attr, color.ReverseVideo)
-		fmt.Printf(" %v ", attr)
-		color.Unset()
-	}
-	fmt.Println()
+	// fmt.Println()
 }
